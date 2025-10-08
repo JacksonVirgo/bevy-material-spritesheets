8a9c6b35ac4bc4c19499dda30b61639f80ec530a
Jack Nash <jacknash42@gmail.com>
2025-10-08 14:56:02 +1000

    scuffed animation

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,24 @@
 use bevy::render::render_resource::AsBindGroup;
 use bevy::{prelude::*, shader::ShaderRef};
+use bevy_material_spritesheets::materials::MaterialTextureAtlas;
 
 fn main() {
     App::new()
         .add_plugins((
             DefaultPlugins.set(ImagePlugin::default_nearest()),
-            MaterialPlugin::<CustomMaterial>::default(),
+            MaterialPlugin::<MaterialTextureAtlas>::default(),
         ))
         .add_systems(Startup, setup)
+        .add_systems(Update, time)
         .run();
-}
-
-#[derive(AsBindGroup, Debug, Clone, Asset, TypePath)]
-struct CustomMaterial {
-    #[uniform(0)]
-    color: LinearRgba,
-    #[texture(1)]
-    #[sampler(2)]
-    texture: Option<Handle<Image>>,
-    alpha_mode: AlphaMode,
-}
-
-impl Material for CustomMaterial {
-    fn fragment_shader() -> ShaderRef {
-        "shader.wgsl".into()
-    }
-    fn alpha_mode(&self) -> AlphaMode {
-        self.alpha_mode
-    }
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut materials: ResMut<Assets<MaterialTextureAtlas>>,
     asset_server: Res<AssetServer>,
 ) {
-    let size = 10.0;
-
-    info!("Here");
-
     commands.spawn((
         Camera::default(),
         Camera3d::default(),
@@ -50,12 +29,47 @@ fn setup(
 
     commands.spawn((
         Mesh3d(meshes.add(Mesh::from(Plane3d::default().mesh().size(100.0, 100.0)))),
-        MeshMaterial3d(materials.add(CustomMaterial {
-            texture: Some(asset_server.load("spritesheet.png")),
-            color: LinearRgba::WHITE,
-            alpha_mode: AlphaMode::Blend,
-        })),
+        MeshMaterial3d(materials.add(MaterialTextureAtlas::from_grid(
+            asset_server.load("spritesheet.png"),
+            UVec2::splat(16),
+            2,
+            2,
+            None,
+            None,
+        ))),
+        TimeStuff { timer: 1.0 },
         Transform::from_xyz(0.0, 0.0, 0.0),
         GlobalTransform::IDENTITY,
     ));
+}
+
+#[derive(Component)]
+pub struct TimeStuff {
+    pub timer: f32,
+}
+
+pub fn time(
+    mut materials: ResMut<Assets<MaterialTextureAtlas>>,
+    mut q: Query<(&MeshMaterial3d<MaterialTextureAtlas>, &mut TimeStuff)>,
+    time: Res<Time>,
+) {
+    for (mat_handle, mut timer) in q.iter_mut() {
+        timer.timer -= time.delta_secs();
+        if timer.timer <= 0.0 {
+            info!("Time");
+            if let Some(mat) = materials.get_mut(&mat_handle.0) {
+                mat.index += 1;
+                if mat.index > 3 {
+                    mat.index = 0;
+                }
+                let i = (mat.index as usize).min(mat.textures.len() - 1);
+                let r = mat.textures[i];
+
+                mat.params.crop_offset_px = Vec2::new(r.min.x as f32, r.min.y as f32);
+                mat.params.crop_size_px =
+                    Vec2::new((r.max.x - r.min.x) as f32, (r.max.y - r.min.y) as f32);
+            }
+            timer.timer = 1.0;
+        }
+    }
 }
